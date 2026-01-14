66c98d09f74b3a072472f0eeccc41e5f8f322075
unknown <bobmoduss@gmail.com>
2026-01-13 23:29:27 -0500

    fix cache issue

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "founder-os-v2";
+const CACHE_NAME = "founder-os-v3";
 const BASE_PATH = "/ToTheMooon/";
 
 const FILES_TO_CACHE = [
@@ -13,16 +13,32 @@ const FILES_TO_CACHE = [
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, clone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
